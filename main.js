7e3119f92bbe09b4bbec603658899f1d3165baff
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-06-13 19:06:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+WebAssembly.instantiateStreaming(fetch(new URL("hello-world.wasm", import.meta.url))).then((obj) => {
+  document.querySelector("#app").innerHTML = "1 + 2 = " + obj.instance.exports.add(1, 2);
+});
